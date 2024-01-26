f85b263a73e0d44fa3392ec8048a655e27a6dbf3
Jacquelyn Kiefner <jacquelyn.kiefner@allata.com>
2024-01-26 14:51:33 -0700

    initial creation of web api, classes, context

---
+++ ToDoWebApi/Controllers/ToDoController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoWebApi.Models;
+
+namespace ToDoWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoController : ControllerBase
+    {
+
+        private readonly ILogger<ToDoController> _logger;
+        private readonly ToDoContext _context;
+
+        public ToDoController(ILogger<ToDoController> logger, ToDoContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetTodos")]
+       public ActionResult<IEnumerable<ToDoItem>> GetTodoItems()
+        {
+            var toDoItems = _context.ToDos.ToList();
+
+            return Ok(toDoItems);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> CreateToDoItem([FromBody] ToDoItem toDoItem)
+        {
+            if (toDoItem == null)
+            {
+                return BadRequest("Invalid Data");
+
+            }
+
+            try
+            {
+                _context.ToDos.Add(toDoItem);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetToDoItem), new { id = toDoItem.Id }, toDoItem);
+            }
+
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("(id)")]
+        public async Task<ActionResult<ToDoItem>> GetToDoItem(int id)
+        {
+            var toDoItem = await _context.ToDos.FindAsync(id);
+
+            if (toDoItem == null)
+            {
+                return NotFound();
+
+            }
+            return Ok(toDoItem);
+        }
+    }
+}
---
+++ ToDoWebApi/ToDoContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoWebApi.Models;
+
+namespace ToDoWebApi
+{
+    public class ToDoContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDos {  get; set; }
+    }
+}
